9751907cbeab6903d4937653ee12391d4086ac8f
Ali-Akber Saifee <ali@indydevs.org>
2015-03-16 15:00:16 +0800

    Upgrade to redis to 2.8.19

--- files/brews/redis.rb
+++ files/brews/redis.rb
@@ -2,10 +2,18 @@ require 'formula'
 
 class Redis < Formula
   homepage 'http://redis.io/'
-  url 'http://download.redis.io/releases/redis-2.8.6.tar.gz'
-  sha1 '8680046580c75987961241f2e1e417c242b91a49'
+  url 'http://download.redis.io/releases/redis-2.8.19.tar.gz'
+  sha1 "3e362f4770ac2fdbdce58a5aa951c1967e0facc8"
 
-  version '2.8.6-boxen1'
+  bottle do
+    sha1 "ba238ce5e71f5c0c3cb997ebda0cf594f75e8069" => :yosemite
+    sha1 "0902233ed41683e22a1ecd8010f2875c9b0b9dba" => :mavericks
+    sha1 "4b8100b40edd0e6ef695e28bf4fd30360939c3f3" => :mountain_lion
+  end
+
+  head "https://github.com/antirez/redis.git", :branch => "unstable"
+
+  version '2.8.19-boxen1'
 
   fails_with :llvm do
     build 2334
@@ -14,13 +22,10 @@ class Redis < Formula
 
   def install
     # Architecture isn't detected correctly on 32bit Snow Leopard without help
-    ENV["OBJARCH"] = MacOS.prefer_64_bit? ? "-arch x86_64" : "-arch i386"
+    ENV["OBJARCH"] = "-arch #{MacOS.preferred_arch}"
 
-    # Head and stable have different code layouts
-    src = (buildpath/'src/Makefile').exist? ? buildpath/'src' : buildpath
-    system "make", "-C", src, "CC=#{ENV.cc}"
+    system "make", "install", "PREFIX=#{prefix}", "CC=#{ENV.cc}"
 
-    %w[benchmark cli server check-dump check-aof].each { |p| bin.install src/"redis-#{p}" }
     %w[run db/redis log].each { |p| (var+p).mkpath }
 
     # Fix up default conf file to match our paths
@@ -30,10 +35,11 @@ class Redis < Formula
       s.gsub! "\# bind 127.0.0.1", "bind 127.0.0.1"
     end
 
+
     # Fix redis upgrade from 2.4 to 2.6.
     if File.exists?(etc/'redis.conf') && !File.readlines(etc/'redis.conf').grep(/^vm-enabled/).empty?
       mv etc/'redis.conf', etc/'redis.conf.old'
-      ohai "Your redis.conf will not work with 2.6; moved it to redis.conf.old"
+      ohai "Your redis.conf will not work with 2.8; moved it to redis.conf.old"
     end
 
     etc.install 'redis.conf' unless (etc/'redis.conf').exist?
